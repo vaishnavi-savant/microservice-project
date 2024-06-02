8f02426e3868e072cb72cceee9aa3f13a5852213
Vaishnavi Bhagwat Savant <98942134+vaishnavi-savant@users.noreply.github.com>
2024-06-02 08:41:07 +0530

    Delete post directory

--- post/src/main/java/com/microservice/post/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.post.repository;
-
-import com.microservice.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, String> {
-
-
-}
